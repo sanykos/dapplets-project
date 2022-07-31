ae836fc643eefd509be602dedf9ffead4f6199ee
Alexander Kosykh <sanykos446@gmail.com>
2022-07-31 15:14:04 +0300

    add normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
