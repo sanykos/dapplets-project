a44423c3c65aefb90d5e554ce6ee2772881bcbda
Alexander Kosykh <sanykos446@gmail.com>
2022-08-01 11:28:36 +0300

    add active item

--- src/components/ItemsList/index.tsx
+++ src/components/ItemsList/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { IData } from '../../store/dapplets/interfaces';
 import { Button } from '../../ui/Button/';
+import classNames from 'classnames';
 import { ReactComponent as LinesIcon } from '../../assets/lines_icon.svg';
 
 import styles from './styles.module.css';
@@ -10,7 +11,8 @@ export const ItemsList: React.FC<{
   isError: boolean;
   items?: Array<IData> | null;
 }> = ({ isError, isLoading, items }) => {
-  const [toggle, setToggle] = useState(false);
+  const [activeItem, setActiveItem] = useState({ id: '', active: false });
+
   return (
     <div className={styles.itemsList}>
       {isError && <p> Ошибка</p>}
@@ -18,21 +20,31 @@ export const ItemsList: React.FC<{
         <p>Загрузка</p>
       ) : (
         items?.map((item) => (
-          <div className={styles.item} key={item.id}>
-            <LinesIcon />
-            <img
-              className={styles.itemIcon}
-              src={`https://dapplets-hiring-api.herokuapp.com/api/v1/files/${item.icon}`}
-              alt="icon"
-            />
+          <div
+            key={item.id}
+            className={classNames(styles.itemContainer, {
+              active: activeItem.id === item.id && activeItem.active,
+            })}
+            onClick={() =>
+              setActiveItem({ id: item.id, active: !activeItem.active })
+            }
+          >
+            <div className={styles.item}>
+              <LinesIcon />
+              <img
+                className={styles.itemIcon}
+                src={`https://dapplets-hiring-api.herokuapp.com/api/v1/files/${item.icon}`}
+                alt="icon"
+              />
 
-            <div className={styles.titleContainer}>
-              <h3>{item.title}</h3>
-              <small>{item.address}</small>
+              <div className={styles.titleContainer}>
+                <h3>{item.title}</h3>
+                <small>{item.address}</small>
+              </div>
+              <p className={styles.itemDescription}>{item.description}</p>
+              <small className={styles.itemAuthor}>{item.author}</small>
+              <Button text="INSTALL" />
             </div>
-            <p className={styles.itemDescription}>{item.description}</p>
-            <small className={styles.itemAuthor}>{item.author}</small>
-            <Button text="INSTALL" />
           </div>
         ))
       )}
